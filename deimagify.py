f1ed3090ba588e4cc27b28157c8da524e764030f
plojyon <yon.ploj@gmail.com>
2020-10-02 19:06:46 +0200

    deimagify no longer requires same working directory

--- deimagify.py
+++ deimagify.py
@@ -2,7 +2,8 @@
 input = input("name of imagified file:");
 output = input.split(".");
 output.pop();
-output = "output_" + ".".join(output);
+output[-2] += "_deimagified";
+output = ".".join(output);
 
 import math;
 from PIL import Image;
@@ -13,9 +14,9 @@ def bitstring2bytearr(s):
 	for i in range(0, len(s), 8):
 		if (i % 2000000 == 0): print("transforming "+str(int((i/int(len(s)))*100))+" % done");
 		sbyte = s[i:i+8];
-		ibyte = int(sbyte, 2)
+		ibyte = int(sbyte, 2);
 		ba.append(ibyte);
-	return ba
+	return ba;
 
 img = Image.open(input);
 size = img.size[0];
@@ -25,7 +26,7 @@ print("decoding ...");
 pixels = img.load(); # create the pixel map
 try:
 	for row in range(size):
-		if (row % 100 == 0): print("decoding "+str(int(100*row/size))+" % done")
+		if (row % 100 == 0): print("decoding "+str(int(100*row/size))+" % done");
 		for col in range(size):
 			if (pixels[col, row][0]-pixels[col, row][1] > 100): # hit red pixel
 				raise IndexError; # break out of nested for loop
